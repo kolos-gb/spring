faaaed4b1ddd29d9e7607ce944f16463465a7f0d
shcherbina-nv <mailto:kolos.book@gmail.com>
2025-01-03 17:38:19 +0300

    homework

---
+++ src/main/java/org/example/spring/model/Basket.java
@@ -0,0 +1,25 @@
+package org.example.spring.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@SessionScope
+public class Basket {
+    List<Integer> order = new ArrayList<>();
+
+    public List<Integer> getOrder() {
+        return order;
+    }
+
+    public void setOrder(List<Integer> order) {
+        this.order = order;
+    }
+
+    public void addOrder(List<Integer> id) {
+        order.addAll(id);
+    }
+}
---
+++ src/main/java/org/example/spring/service/OrderService.java
@@ -0,0 +1,10 @@
+package org.example.spring.service;
+
+import java.util.List;
+
+public interface OrderService {
+
+    String add(List<Integer> list);
+
+    List<Integer> get();
+}
